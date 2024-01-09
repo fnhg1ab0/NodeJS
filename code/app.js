58bbbdaa190f05eb65a0baef551e2500835dfef0
fnhg1ab0 <thinhduc12345@gmail.com>
2024-01-09 19:21:54 +0700

    authentication

--- code/app.js
+++ code/app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const sessionStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -14,9 +16,11 @@ const MongoDbUrl = 'mongodb://localhost:27017/nodejs';
 const app = express();
 
 const store = new sessionStore({
-    uri: MongoDbUrl,
+    url: MongoDbUrl,
     collection: 'sessions'
 })
+
+const csrfProtect = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -28,7 +32,8 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({secret: 'nccm8&zc%', resave: false, saveUninitialized: false, store: store}));
-
+app.use(csrfProtect);
+app.use(flash());
 app.use((req, res, next) => {
     if (!req.session.user) return next();
     User.findById(req.session.user._id)
@@ -38,6 +43,13 @@ app.use((req, res, next) => {
         })
         .catch(err => console.log(err));
 });
+
+//  tell expressjs the data that should be included in every rendered view
+app.use((req, res, next) => {
+    res.locals.isAuth = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -50,18 +62,6 @@ mongoose
         MongoDbUrl
     )
     .then(result => {
-        User.findOne().then(user => {
-            if (!user) {
-                const user = new User({
-                    name: 'Tuan',
-                    email: 'songgio@gmail.com',
-                    cart: {
-                        items: []
-                    }
-                });
-                user.save();
-            }
-        });
         app.listen(3000);
     })
     .catch(err => {
@@ -70,3 +70,6 @@ mongoose
 
 // npm install --save express-session
 // npm install --save connect-mongodb-session
+// npm install --save bcryptjs
+// npm install --save csurf
+// npm install --save connect-flash
--- code/controllers/auth.js
+++ code/controllers/auth.js
@@ -1,30 +1,84 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user')
 
 exports.getLogin = (req, res, next) => {
+    let mess = req.flash('error');
+    if (mess.length > 0) mess = mess[0];
+    else mess = null;
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuth: req.session.isLoggedIn
+        errMess: mess
     })
 }
 
 exports.postLogin = (req, res, next) => {
+    const email = req.body.email;
+    const pass = req.body.password;
     // res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
-    User.findById('6566ea89bddf3e67f3bfcee9')
+    User.findOne({email: email})
         .then(user => {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            res.session.save((err) => { // ensure session is saved in database before redirect
-                console.log(err);
-                res.redirect('/');
-            })
+            if (!user) {
+                req.flash('error', 'Email does not exist!');
+                return res.redirect('/login');
+            }
+            bcrypt.compare(pass, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        req.session.isLoggedIn = true;
+                        req.session.user = user;
+                        return req.session.save((err) => { // ensure session is saved in database before redirect
+                            res.redirect('/');
+                        });
+                    }
+                    req.flash('error', 'Wrong password!');
+                    res.redirect('/login');
+                })
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            res.redirect('/login');
+        });
 }
 
 exports.postLogout = (req, res, next) => {
     req.session.destroy(err => {
-        console.log(err);
         res.redirect('/');
     });
 }
+
+exports.getSignup = (req, res, next) => {
+    let mess = req.flash('error');
+    if (mess.length > 0) mess = mess[0];
+    else mess = null;
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        errMess: mess
+    });
+};
+
+exports.postSignup = (req, res, next) => {
+    const email = req.body.email;
+    const pass = req.body.password;
+    const confirm = req.body.confirmPassword;
+
+    User.findOne({email: email})
+        .then(user => {
+            if (user) {
+                req.flash('error', 'Email already exists!');
+                return res.redirect('/signup');
+            }
+            return bcrypt.hash(pass, 12)
+                .then((hashPass) => {
+                    const newUser = new User({
+                        email: email,
+                        password: hashPass,
+                        cart: {items: []}
+                    });
+                    return newUser.save();
+                })
+                .then(user => res.redirect('/login'));
+        })
+        .catch(err => console.log(err));
+};
