fae1db2ff2864f89f85dcb93301a6f6779a07c3d
fnhg1ab0 <thinhduc12345@gmail.com>
2024-01-12 14:59:10 +0700

    pagination + asyc request

---
+++ code/public/js/admin.js
@@ -0,0 +1,16 @@
+const deleteProduct = (btn) => {
+    const prodId = btn.parentNode.querySelector('[name=productId]').value;
+    const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+    const productElement = btn.closest('article');
+
+    fetch('/admin/product/' + prodId, {
+        method: 'DELETE',
+        headers: {'csrf-token': csrf}
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            productElement.remove();
+        })
+        .catch(err => console.log(err));
+}
